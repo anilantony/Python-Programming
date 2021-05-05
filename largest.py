4c794d888614321f4df8d8b7870386633762a0dc
anil antony <anil@jecc.ac.in>
2021-05-05 14:41:26 +0530

    Create largest.py
    for finding the largest number in a list

---
+++ largest.py
@@ -0,0 +1,8 @@
+#finding the largest value
+largest=-1
+print('Before',largest)
+for num in [9,41,12,3,74,15]:
+    if num>largest:
+        largest=num
+    print(largest,num)
+print('after',largest)
